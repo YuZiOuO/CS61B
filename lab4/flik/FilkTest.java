5a2afe940fad2907f651eb31901162d8d86b3cff
Kanata <40749962+YuZiOuO@users.noreply.github.com>
2024-10-09 17:03:14 +0800

    lab4: add tests for Filk

---
+++ lab4/flik/FilkTest.java
@@ -0,0 +1,19 @@
+package flik;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FilkTest {
+    static int MAX_TRIES = 1000000;
+    @Test
+    public void randomTest() {
+        for (int i = 0; i < MAX_TRIES; i++) {
+            int a = StdRandom.uniform(0,MAX_TRIES);
+            int b = StdRandom.uniform(0,MAX_TRIES);
+            String s = a + " " + b;
+            assertEquals(s,a-b==0?true:false,Flik.isSameNumber(a,b));
+        }
+    }
+}
