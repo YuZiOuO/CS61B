c2bf59ec5f012fe7e17764bd51aac5bdf85d4c6e
Kanata <40749962+YuZiOuO@users.noreply.github.com>
2024-12-11 18:31:43 +0800

    lab12: done

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -1,15 +1,60 @@
 package byow.lab12;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
-import java.util.Random;
-
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public static final int WIDTH = 60;
+    public static final int HEIGHT = 30;
+
+    public static void main(String[] args) {
+        TERenderer renderer = new TERenderer();
+        renderer.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        addAHex(world, 10, 10, 4, Tileset.FLOOR);
+        addAHex(world, 17, 14, 4, Tileset.FLOWER);
+        renderer.renderFrame(world);
+    }
+
+    /**
+     * Add a Hex with given size whose top left vertex is at (x,y)
+     */
+    private static void addAHex(TETile[][] tiles, int x, int y, int size, TETile tile) {
+        double axis = y - size + 0.5;
+        for (int i = 0; i < size; i++) {
+            drawALine(tiles, x - i, y - i, size + 2 * i, tile, axis);
+        }
+    }
+
+    private static void drawALine(TETile[][] tiles,
+                                  int x, int y, int size, TETile tile,
+                                  double axis) {
+        for (int i = 0; i < size; i++) {
+            tiles[x + i][y] = tile;
+            axialSymmetry(tiles, x + i, y, axis);
+        }
+    }
+
+    private static void lineSymmetry(TETile[][] tiles, int x, int y, double axis) {
+        // symmetrify like this to avoid redundant computation for symmetric y.
+    }
+
+    private static void axialSymmetry(TETile[][] tiles,
+                                      int x, int y,
+                                      double axis) {
+        double yDiff = Math.abs(y - axis);
+        int symY = y > axis ? (int) (axis - yDiff) : (int) (axis + yDiff);
+        tiles[x][symY] = tiles[x][y];
+    }
 }
